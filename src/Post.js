3752945ad95fd9607c518c63f962599327bff881
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-10 07:38:44 +0700

    add title, creator and content

--- src/Post.js
+++ src/Post.js
@@ -13,6 +13,14 @@ function Post({post}) {
     return (
         <div className="post">
             <img className="post-image" src={imageUrl}></img>
+            <div className="post-content">
+                <div className="post-title">{post.title}</div>
+                <div className="post-creator">by {post.creator}</div>
+                <div className="post-text">{post.content}</div>
+                <div className="post-delete">
+                    <button className="btn-delete" onClick={null}>Delete Post</button>
+                </div>
+            </div>
         </div>
     )
 }
